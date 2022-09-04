567b21ae6ca5ce331437106e5174b1eae4598d4b
edwardmiller123 <edwardomiller123@gmail.com>
2022-09-04 12:13:07 +0100

    incorrect password returns to previous stage

--- dataBase.go
+++ dataBase.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -32,9 +33,11 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	var selector string
 	dataBase := []person{}
+
 	for selector != "end" {
+
 		for selector != "1" && selector != "2" && selector != "3" && selector != "end" {
-			fmt.Printf("Press 1 to register new person.\nPress 2 to look up existing Person\nPress 3 to edit existing entry.\n")
+			fmt.Printf("Press 1 to register new person.\nPress 2 to look up existing Person\nPress 3 for admin options.\n")
 			fmt.Scan(&selector)
 		}
 		if selector == "1" {
@@ -54,6 +57,7 @@ func main() {
 
 		} else if selector == "2" {
 			var selector2 string
+
 			for selector2 != "1" && selector2 != "2" && selector2 != "end" {
 				fmt.Printf("To search by name or favourite animal press 1.\nTo search by age press 2.\n")
 				fmt.Scan(&selector2)
@@ -68,6 +72,7 @@ func main() {
 				fmt.Scan(&search)
 
 				for i := 0; i < len(dataBase); i++ {
+
 					if dataBase[i].name == search || dataBase[i].favAnimal == search {
 						fmt.Println(dataBase[i])
 					}
@@ -81,14 +86,79 @@ func main() {
 				fmt.Scan(&searchAge)
 
 				for j := 0; j < len(dataBase); j++ {
+
 					if dataBase[j].age == searchAge {
 						fmt.Println(dataBase[j])
 					}
 				}
 			}
 		} else if selector == "3" {
-			fmt.Println(dataBase)
-			selector = "0"
+			var password string
+			tries := 0
+
+			for password != "badpassword123" {
+				fmt.Printf("Enter password: ")
+				fmt.Scan(&password)
+
+				if tries == 2 {
+					fmt.Println("One attempt remaining!")
+				} else if tries == 3 {
+					break
+				}
+
+				tries++
+			}
+			var selector3 string
+
+			if password != "badpassword123" {
+				selector = "0"
+				selector3 = "return"
+			}
+
+			for selector3 != "1" && selector3 != "2" && selector3 != "return" && selector3 != "end" {
+				fmt.Println(dataBase)
+				fmt.Printf("To edit existing entry, press 1.\nTo return, type \"return\".\n")
+				fmt.Scan(&selector3)
+
+			}
+			if selector3 == "end" {
+				selector = "end"
+
+			} else if selector3 == "return" {
+				selector = "0"
+
+			} else if selector3 == "1" {
+				var idToChange int
+				var propertyToChange, newValue string
+
+				fmt.Println("Enter Id number of entry, the property to be modifed and the new value of that property:")
+				fmt.Scan(&idToChange, &propertyToChange, newValue)
+
+				for n := 0; n < len(dataBase); n++ {
+
+					if dataBase[n].idNumber == idToChange {
+
+						if propertyToChange == "name" {
+							dataBase[n].name = newValue
+
+						} else if propertyToChange == "age" {
+							newValueInt, err := strconv.Atoi(newValue)
+
+							if err != nil {
+								// ... handle error
+								panic(err)
+							}
+							dataBase[n].age = newValueInt
+						} else if propertyToChange == "favourite animal" {
+							dataBase[n].favAnimal = newValue
+						}
+					}
+				}
+				fmt.Println(dataBase)
+				selector = "3"
+
+			}
+
 		}
 
 	}
